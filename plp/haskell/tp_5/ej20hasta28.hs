59a54210118adfd05a8bcaecfee767cad340367a
mati_degli <matias.degli@gmail.com>
2025-04-28 20:03:49 -0300

    nuevos

---
+++ plp/haskell/tp_5/ej20hasta28.hs
@@ -0,0 +1,13 @@
+maxOcurr :: Int -> Int -> (Int,Int)
+maxOcurr a b
+    | a > b = (a,1)
+    | a == b = (a,2)
+    | otherwise = (b,1)
+
+maxOcurr3 :: Int -> Int -> Int -> (Int, Int)
+maxOcurr3 a b c = (mayor, cantidad)
+    where
+        (mayorAB, _) = maxOcurr a b
+        (mayor, _) = maxOcurr mayorAB c
+        cantidad = length (filter (== mayor) [a, b, c])
+
