ef9c97bb1a223a383e2f8c63aa94584dda97c270
mati_degli <matias.degli@gmail.com>
2025-04-28 22:14:43 -0300

    ej24

--- plp/haskell/tp_5/ej20hasta28.hs
+++ plp/haskell/tp_5/ej20hasta28.hs
@@ -1,3 +1,7 @@
+{-
+ejercicio 20
+-}
+
 maxOcurr :: Int -> Int -> (Int,Int)
 maxOcurr a b
     | a > b = (a,1)
@@ -11,3 +15,18 @@ maxOcurr3 a b c = (mayor, cantidad)
         (mayor, _) = maxOcurr mayorAB c
         cantidad = length (filter (== mayor) [a, b, c])
 
+{-
+ejercicio 24
+-}
+
+cadenaDigitos :: Int -> String
+cadenaDigitos n
+
+
+repeticion :: Int -> Int
+repeticion n = n*10
+
+sumatoria :: Int -> Int -> Int -> Int
+sumatoria i n m 
+    | i > 1 = sumatoria i-1 (n + repeticion m) (repeticion m)
+    | otherwise = n
