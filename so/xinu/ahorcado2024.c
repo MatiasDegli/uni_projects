f3c8c500d063991969a690f49538b72c6663ec14
MatiasDegli <matias.degli@gmail.com>
2025-05-02 16:04:11 -0300

    ahorcado2

--- so/xinu/ahorcado2024.c
+++ so/xinu/ahorcado2024.c
@@ -6,42 +6,72 @@ void computo(void);
 void visual(void);
 void fin(void);
 
-char palabra[] = "linux", adivinado[27];
+char palabra[] = "linux";
+const int longitud = sizeof(palabra)-1;
+char adivinado[27], actual[80];
+
 int n; // cant letras
+
 pid32 epid, cpid;
 
 void entrada(void){
     
+    epid = getpid();
     int c;
     control(CONSOLE, TC_MODER, 0, 0);
 
     while(1){
 
         c = getc(CONSOLE);
-
         send(cpid, c);
+        receive();
         
     }
+
+    control(CONSOLE, TC_MODEC, 0, 0);
 
 }
 
 void computo(void){
 
+    cpid = getpid();
+
     while(1){
 
-        char c = receive();
+        char c = (char) receive();
         int esta = 0; 
 
         for (int i = 0; i < n; i++){
-            if(c == adivinado[i]){
+            if(adivinado[i] == '\0'){ // Se llega al final de la cadena
+                break;
+            }
+            else if(c == adivinado[i]){
                 esta = 1;
                 break;
             }
         }
         
-        if(palabra[i]){
+        if(esta == 0){
+            
+            for (i = 0; i < longitud; i++)
+            {
+                if(){
 
+                }
+            }
+            
         }
+        else{
+            adivinado[n] = c;
+            n++;
+            adivinado[n] = '\0';
+        }
+
+        send(epid, 1);
+    }
+
+    void ahorcado(void){
+        
     }
 
 }
